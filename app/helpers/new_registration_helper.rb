af7e4253ee75aecf70ef06054cb94337540f4c82
Vivian Canales <vcnales@gmail.com>
2021-10-12 15:30:40 -0500

    initial commit

---
+++ app/helpers/new_registration_helper.rb
@@ -0,0 +1,2 @@
+module NewRegistrationHelper
+end
---
+++ test/controllers/new_registration_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class NewRegistrationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
